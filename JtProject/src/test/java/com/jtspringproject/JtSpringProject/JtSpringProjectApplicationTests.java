11aa914d2e76803493a0ab8483fc656233fd35f5
AyeshaHaroon394 <102833960+AyeshaHaroon394@users.noreply.github.com>
2023-12-03 23:09:46 +0500

    Commit By Ayesha
    JUnit Testing added for bugs fixed

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
@@ -1,13 +1,102 @@
 package com.jtspringproject.JtSpringProject;
 
+import com.jtspringproject.JtSpringProject.controller.UserController;
+import com.jtspringproject.JtSpringProject.models.User;
+import com.jtspringproject.JtSpringProject.services.userService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import com.jtspringproject.JtSpringProject.controller.UserController;
+import com.jtspringproject.JtSpringProject.models.User;
+
+import com.jtspringproject.JtSpringProject.services.userService;
+import org.junit.Before;
 
 @SpringBootTest
 class JtSpringProjectApplicationTests {
 
+	private UserController userController;
+	private userService userServices;
+	private RedirectAttributes redirectAttributes;
+
+	@BeforeEach
+	public void setUp() {
+		userServices = mock(userService.class);
+		userController = new UserController(userServices);
+		redirectAttributes = new RedirectAttributesModelMap();
+	}
+
 	@Test
-	void contextLoads() {
+	public void testNewUserRegisterWithBlankUsername() {
+		User user = new User();
+		user.setUsername(""); // Blank username
+
+		ModelAndView modelAndView = userController.newUseRegister(user, redirectAttributes);
+
+		assertEquals("redirect:/BlankUsername", modelAndView.getViewName());
+	}
+
+	@Test
+	public void testNewUserRegisterWithExistingUsername() {
+		User user = new User();
+		user.setUsername("existingUser");
+
+		when(userServices.isUserExists("existingUser")).thenReturn(true);
+
+		ModelAndView modelAndView = userController.newUseRegister(user, redirectAttributes);
+
+		assertEquals("redirect:/ExistsUsernameError", modelAndView.getViewName());
+	}
+
+	@Test
+	public void testNewUserRegisterWithWeakPassword() {
+		User user = new User();
+		user.setUsername("newUser");
+		user.setPassword("weak"); // Weak password
+
+		ModelAndView modelAndView = userController.newUseRegister(user, redirectAttributes);
+
+		assertEquals("redirect:/PasswordError", modelAndView.getViewName());
+	}
+
+	@Test
+	public void testNewUserRegisterWithExistingEmail() {
+		User user = new User();
+		user.setUsername("newUser");
+		user.setEmail("existing@email.com");
+		user.setPassword("1234abcd");
+
+		when(userServices.isEmailExists(user.getEmail())).thenReturn(true);
+
+		ModelAndView modelAndView = userController.newUseRegister(user, redirectAttributes);
+
+		assertEquals("redirect:/ExistsEmailError", modelAndView.getViewName());
+	}
+
+	@Test
+	public void testNewUserRegisterSuccess() {
+		User user = new User();
+		user.setUsername("newUser");
+		user.setPassword("StrongPassword123");
+		user.setEmail("new@email.com");
+
+		when(userServices.isUserExists(anyString())).thenReturn(false);
+		when(userServices.isEmailExists(anyString())).thenReturn(false);
+
+		ModelAndView modelAndView = userController.newUseRegister(user, redirectAttributes);
+
+		assertEquals("redirect:/userLogin1", modelAndView.getViewName());
 	}
 
 }
