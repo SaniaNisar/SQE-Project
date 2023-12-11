c4b0383a7595df055d2bda8769ddb284b30beac1
DuaMohyyuddin <102690488+DuaMohyyudin@users.noreply.github.com>
2023-12-11 21:43:46 +0500

    Comit by dua (User LogIn Validation test functions added)

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/controller/UserControllerTest.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/controller/UserControllerTest.java
@@ -87,4 +87,37 @@ public class UserControllerTest {
 
         assertEquals("redirect:/userLogin1", modelAndView.getViewName());
     }
+
+        @Test
+    public void testUserLoginValidateSuccess() {
+        String username = "testUser";
+        String password = "testPassword";
+        Model model = mock(Model.class);
+        ModelAndView expectedView = new ModelAndView("index");
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        when(userService.checkLogin(username, password)).thenReturn(user);
+
+        ModelAndView result = controller.userlogin(username, password, model, null);
+
+        assertEquals(expectedView.getViewName(), result.getViewName());
+        assertEquals(user, result.getModel().get("user"));
+    }
+
+    @Test
+    public void testUserLoginValidateFailure() {
+        String username = "invalidUser";
+        String password = "invalidPassword";
+        Model model = mock(Model.class);
+        ModelAndView expectedView = new ModelAndView("userLogin");
+
+        when(userService.checkLogin(username, password)).thenReturn(new User());
+
+        ModelAndView result = controller.userlogin(username, password, model, null);
+
+        assertEquals(expectedView.getViewName(), result.getViewName());
+        assertEquals("Please enter correct email and password", result.getModel().get("msg"));
+    }
 }
