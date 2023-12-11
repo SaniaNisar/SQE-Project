3b2768d2c2ec9a6cdc1310c9a4d6f987536280a1
AyeshaHaroon394 <102833960+AyeshaHaroon394@users.noreply.github.com>
2023-12-11 19:20:18 +0500

    Commit By Ayesha - Update userService.java

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/services/userService.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/services/userService.java
@@ -26,5 +26,15 @@ public class userService {
 		return this.userDao.getUser(username, password);
 	}
 
+	public boolean isUserExists(String username) 
+	{
+		return this.userDao.isUserExists(username);
+        }
+
+	public boolean isEmailExists(String email)
+	{
+		return this.userDao.isEmailExists(email);
+	}
+
 	
 }
