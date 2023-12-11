7aebeabe807faf2271f1ea01a4002575f813358c
Sania Nisar <102464125+SaniaNisar@users.noreply.github.com>
2023-12-11 22:38:49 +0500

    Commit By Sania - test for bug Fix

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/controller/UserControllerTest.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/controller/UserControllerTest.java
@@ -12,6 +12,18 @@ import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class UserControllerTest {
 
@@ -87,6 +99,86 @@ public class UserControllerTest {
 
         assertEquals("redirect:/userLogin1", modelAndView.getViewName());
     }
+    @Test
+    public void testGetUserDetailsWithValidSession() throws Exception {
+        // Mocking dependencies
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
 
+        // Creating a user and session
+        User mockUser = new User("mockUsername", "mockPassword");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("currentUsername", "mockUsername");
+        session.setAttribute("currentpass", "mockPassword");
+
+        // Mocking userService behavior
+        when(userService.checkLogin("mockUsername", "mockPassword")).thenReturn(mockUser);
+
+        // Setting up the MockMvc
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+
+        // Performing the GET request
+        mockMvc.perform(get("/user/profile").session(session))
+                .andExpect(status().isOk())
+                .andExpect(view().name("profile"))
+                .andExpect(model().attributeExists("user"))
+                .andExpect(model().attribute("user", mockUser));
+    }
+
+    @Test
+    public void testGetUserDetailsWithMissingSessionAttributes() throws Exception {
+        // Mocking dependencies
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        // Creating a session without required attributes
+        MockHttpSession session = new MockHttpSession();
+
+        // Setting up the MockMvc
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+
+        // Performing the GET request
+        mockMvc.perform(get("/user/profile").session(session))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+    }
+
+    @Test
+    public void testGetUserDetailsWithInvalidUser() throws Exception {
+        // Mocking dependencies
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        // Creating a session with valid attributes
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("currentUsername", "mockUsername");
+        session.setAttribute("currentpass", "mockPassword");
+
+        // Mocking userService behavior for an invalid user
+        when(userService.checkLogin("mockUsername", "mockPassword")).thenReturn(null);
+
+        // Setting up the MockMvc
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+
+        // Performing the GET request
+        mockMvc.perform(get("/user/profile").session(session))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+    }
+
+    @Test
+    public void testUserLogin() throws Exception {
+        // Mocking dependencies
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        // Setting up the MockMvc
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+
+        // Performing the GET request
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("userLogin"));
+    }
 
 }
